72656b2b066240093170105b6bb01ff7663cd9db
Miklos Komlosy <2220859001@fh-burgenland.at>
2024-10-24 12:39:11 +0200

    feat: setting up for springboot with gradle

---
+++ src/main/java/com/Team16/raspberry_pi_sensor/RaspberryPiSensorApplication.java
@@ -0,0 +1,13 @@
+package com.Team16.raspberry_pi_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPiSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryPiSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Team16/raspberry_pi_sensor/RaspberryPiSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team16.raspberry_pi_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryPiSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
